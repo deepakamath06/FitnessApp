1fb64732daff3bcfc84dd6a4539c1ecf2dc47fb6
dkamath <deepajkamath@gmail.com>
2025-08-04 11:48:16 -0700

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Deepa Kamath on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
