d635fb1fed9f1e5032c6df38ad4a8f3136f165c1
dkamath <deepajkamath@gmail.com>
2025-08-12 16:17:49 -0700

    HomeView Completed and separation of files into Model View and View Model

---
+++ FitnessApp/Home/Models/Activity.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityView.swift
+//  FitnessApp
+//
+//  Created by Deepa Kamath on 8/12/25.
+//
+
+import Foundation
+import SwiftUI
+struct Activity{
+    let id: Int
+    let title: String
+    let subtitle: String
+    let image: String
+    let tintColor: Color
+    let amount: String
+}
---
+++ FitnessApp/Home/Models/Workout.swift
@@ -0,0 +1,17 @@
+//
+//  Workout.swift
+//  FitnessApp
+//
+//  Created by Deepa Kamath on 8/12/25.
+//
+
+import SwiftUI
+struct Workout{
+    let id: Int
+    let title: String
+    let image: String
+    let tintcolor: Color
+    let duration: String
+    let date: String
+    let calories:String
+}
